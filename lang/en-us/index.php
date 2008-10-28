01b895d902ea6a57461a29e2dda8006d56ec9023
Leandro Perrotta <leandro_perrotta@hotmail.com>
2008-10-28 23:22:44 +0000

    *Removido antiga trunk
    git-svn-id: svn://darghos.com.br/darghos/website/trunk@145 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- lang/en-us/index.php
+++
@@ -1,6 +0,0 @@
-<?
-	ob_start();
-	session_start();
-	$_SESSION['lang'] = 'en_us';
-	header ("location: ../../index.php");
-?>
--- lang/pt-br/index.php
+++
@@ -1,6 +0,0 @@
-<?
-	ob_start();
-	session_start();
-	$_SESSION['lang'] = 'pt_br';
-	header ("location: ../../index.php");
-?>
